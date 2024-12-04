5ebf39bd63620246143b111cebc45d8e3c2cde8b
MOR-E <pedrohenrique88977944@gmail.com>
2024-12-04 17:58:56 -0300

    firts commit

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+
+const express = require('express')
+const path = require('path')
+const cors = require('cors')
+
+const port = process.env.PORT
+
+const app = express()
+
+// config json and form data response
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+// Solve CORS
+app.use(cors({credentials: true, origin: "http://localhost:5173"}))
+
+//routes
+const router = require('./routes/Router');
+
+app.use(router)
+
+
+app.listen(port,() => {
+    console.log(`app rodando na porta ${port}`)
+})
